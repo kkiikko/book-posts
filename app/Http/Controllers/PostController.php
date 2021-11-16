0e62314f0df0d595c3a4a6d28184736aa9d1b471
kkiikko <kainosuke1234@gmail.com>
2021-11-16 19:00:15 +0900

    add edit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,4 +29,15 @@ class PostController extends Controller
         
     }
     
+    public function edit(Post $post)
+    {
+        return view('/posts/edit')->with(['post'=>$post]);
+    }
+    
+    public function update(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,7 @@
         <h4>{{$post->title}}</h4>
         <h4>review<review>
         <h4>{{$post->body}}</h4>
-      
+       <p class="edit">[<a href="/posts/{{$post->id}}/edit">編集</a>]</p>
       <div class="footer">
           <a href="/">back</a>
 </body>
